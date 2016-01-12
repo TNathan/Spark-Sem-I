92aa20caff243b0abe29abb58d65f868a3cd0114
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-01-12 13:50:20 +0100

    Initial Scala structure.

---
+++ src/main/scala/org/dissect/inference/Test.scala
@@ -0,0 +1,20 @@
+package org.dissect.inference
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * @author Lorenz Buehmann
+  *
+  */
+object Test {
+  def main(args: Array[String]) {
+    val logFile = "README.md" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application")
+    conf.setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
+}
