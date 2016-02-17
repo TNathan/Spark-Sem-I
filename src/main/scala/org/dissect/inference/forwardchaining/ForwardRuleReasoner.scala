099a80e1ab18626f2db52a1a174f5a881c7a11f5
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-02-17 14:27:23 +0100

    Compute transitive closure of RDD of triples.

--- src/main/scala/org/dissect/inference/forwardchaining/ForwardRuleReasoner.scala
+++ src/main/scala/org/dissect/inference/forwardchaining/ForwardRuleReasoner.scala
@@ -1,7 +1,7 @@
 package org.dissect.inference.forwardchaining
 
 import org.apache.spark.rdd.RDD
-import org.dissect.inference.data.RDFGraph
+import org.dissect.inference.data.{RDFTriple, RDFGraph}
 
 import scala.collection.mutable
 import org.apache.spark.SparkContext._
@@ -9,6 +9,8 @@ import org.apache.spark.SparkContext._
 import scala.reflect.ClassTag
 
 /**
+  * A forward chaining based reasoner.
+  *
   * @author Lorenz Buehmann
   */
 trait ForwardRuleReasoner {
@@ -32,33 +34,33 @@ trait ForwardRuleReasoner {
     s ++ (for ((s1, p1, o1) <- s; (s2, p2, o2) <- s if o1 == s2) yield (s1, p1, o2))
   }
 
-//  def computeTransitiveClosure[A, B](triples: RDD[(A, B, A)]): RDD[(A, B, A)] = {
-//    // keep the predicate
-//    val predicate = triples.take(1)(0)._2
-//
-//    // compute the TC
-//    var subjectObjectPairs = triples.map(t => (t._1, t._3)).cache()
-//
-//    // because join() joins on keys, in addition the pairs are stored in reversed order (o, s)
-//    val objectSubjectPairs = subjectObjectPairs.map(t => (t._2, t._1))
-//
-//    // the join is iterated until a fixed point is reached
-//    var oldCount = 0L
-//    var nextCount = triples.count()
-//    do {
-//      oldCount = nextCount
-//      // perform the join (s1, o1) x (o2, s2), obtaining an RDD of (s1=o2, (o1, s2)) pairs,
-//      // then project the result to obtain the new (s2, o1) paths.
-//      subjectObjectPairs = subjectObjectPairs
-//        .union(subjectObjectPairs.join(objectSubjectPairs).map(x => (x._2._2, x._2._1)))
-//        .distinct()
-//        .cache()
-//      nextCount = subjectObjectPairs.count()
-//    } while (nextCount != oldCount)
-//
-//    println("TC has " + subjectObjectPairs.count() + " edges.")
-//    subjectObjectPairs.map(p => (p._1, predicate, p._2))
-//  }
+  def computeTransitiveClosure(triples: RDD[RDFTriple]): RDD[RDFTriple] = {
+    // keep the predicate
+    val predicate = triples.take(1)(0).predicate
+
+    // compute the TC
+    var subjectObjectPairs = triples.map(t => (t.subject, t.obj)).cache()
+
+    // because join() joins on keys, in addition the pairs are stored in reversed order (o, s)
+    val objectSubjectPairs = subjectObjectPairs.map(t => (t._2, t._1))
+
+    // the join is iterated until a fixed point is reached
+    var oldCount = 0L
+    var nextCount = triples.count()
+    do {
+      oldCount = nextCount
+      // perform the join (s1, o1) x (o2, s2), obtaining an RDD of (s1=o2, (o1, s2)) pairs,
+      // then project the result to obtain the new (s2, o1) paths.
+      subjectObjectPairs = subjectObjectPairs
+        .union(subjectObjectPairs.join(objectSubjectPairs).map(x => (x._2._2, x._2._1)))
+        .distinct()
+        .cache()
+      nextCount = subjectObjectPairs.count()
+    } while (nextCount != oldCount)
+
+    println("TC has " + subjectObjectPairs.count() + " triples.")
+    subjectObjectPairs.map(p => new RDFTriple(p._1, predicate, p._2))
+  }
 
   def computeTransitiveClosure[A:ClassTag](edges: RDD[(A, A)]): RDD[(A, A)] = {
     // we keep the transitive closure cached
@@ -100,9 +102,9 @@ trait ForwardRuleReasoner {
   /**
     * Extracts all triples for the given predicate.
     *
-    * @param triples the triples
+    * @param triples the RDD of triples
     * @param predicate the predicate
-    * @return the set of triples that contain the predicate
+    * @return the RDD of triples that contain the predicate
     */
   def extractTriples(triples: RDD[(String, String, String)], predicate: String): RDD[(String, String, String)] = {
     triples.filter(triple => triple._2 == predicate)
