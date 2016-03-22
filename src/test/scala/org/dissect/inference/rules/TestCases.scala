298374a1344da8885a4be80245eeef6b22576dc0
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-03-22 14:01:58 +0100

    Added RDFS conformance tests.

--- src/test/scala/org/dissect/inference/rules/TestCases.scala
+++ src/test/scala/org/dissect/inference/rules/TestCases.scala
@@ -2,23 +2,58 @@ package org.dissect.inference.rules
 
 import java.io.File
 
+import org.apache.jena.riot.RDFDataMgr
+
+import scala.collection.mutable.ListBuffer
 import scala.xml.XML
 
 /**
   * @author Lorenz Buehmann
   */
-class TestCases {
+object TestCases {
 
-  def loadTestCases(directory: File) = {
+  /**
+    * Loads test cases from the given root folder.
+    *
+    * @param directory the root folder containing sub-folders for each test case
+    * @return test cases
+    */
+  def loadTestCases(directory: File): Seq[TestCase] = {
 
-    directory.listFiles().filter(f => f.isDirectory).foreach { subDirectory =>
+    val testCases = new ListBuffer[TestCase]()
 
-      val metadataFile = subDirectory.listFiles().filter(_.getName.endsWith(".metadata.properties")).toList.head
+    directory.listFiles().filter(_.isDirectory).foreach { subDirectory =>
 
+      // the files in the directory
+      val files = subDirectory.listFiles()
+
+      // get the metadata file
+      val metadataFile = files.filter(_.getName.endsWith(".metadata.properties")).head
+
+      // load metadata XML
       val metadata = XML.loadFile(metadataFile)
 
-      val isEntailmentTestCase = (metadata \\ "entry").filter(n => n.attribute("key").get.text == "testcase.type").text == "POSITIVE_ENTAILMENT"
+      // id
+      val id = (metadata \\ "entry").filter(n => n.attribute("key").get.text == "testcase.id").text
 
+      // description
+      val description = (metadata \\ "entry").filter(n => n.attribute("key").get.text == "testcase.description").text
+
+      // test case type
+      val entailmentType = (metadata \\ "entry").filter(n => n.attribute("key").get.text == "testcase.type").text
+
+      // currently we support only entailment test cases
+      if(entailmentType == "POSITIVE_ENTAILMENT") {
+        // load input data
+        val inputGraph = RDFDataMgr.loadModel(files.filter(_.getName.endsWith(".premisegraph.ttl")).head.getPath)
+
+        // load output data
+        val outputGraph = RDFDataMgr.loadModel(files.filter(_.getName.endsWith(".conclusiongraph.ttl")).head.getPath)
+
+        testCases += TestCase(id, description, entailmentType, inputGraph, outputGraph)
+      }
     }
+
+    testCases.toSeq
   }
 }
