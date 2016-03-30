9611ee4602910460009d6484b7e7a9123246a325
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-03-30 14:56:17 +0200

    Find operation on graph.

--- src/main/scala/org/dissect/inference/data/RDFGraph.scala
+++ src/main/scala/org/dissect/inference/data/RDFGraph.scala
@@ -1,5 +1,6 @@
 package org.dissect.inference.data
 
+import org.apache.jena.graph.{Node, NodeFactory, Triple}
 import org.apache.spark.rdd.RDD
 
 /**
@@ -9,5 +10,30 @@ import org.apache.spark.rdd.RDD
   *
   */
 case class RDFGraph (triples: RDD[RDFTriple]) {
+
+  /**
+    * Returns an RDD of triples that match with the given input.
+    *
+    * @param s the subject
+    * @param p the predicate
+    * @param o the object
+    * @return RDD of triples
+    */
+  def find (s: Option[String] = None, p: Option[String] = None, o: Option[String] = None): RDD[RDFTriple]= {
+      triples.filter(t =>
+          (s == None || t.subject == s) &&
+          (p == None || t.predicate == p) &&
+          (o == None || t.`object` == o)
+      )
+  }
+
+  def find (triple: Triple): RDD[RDFTriple]= {
+    find(Option(triple.getSubject.toString), Option(triple.getPredicate.toString), Option(triple.getObject.toString))
+  }
+
+//  def find (s: Node, p: Node, o: Node) = {
+//    val targetTriple = Triple.createMatch(s, p, o)
+//    triples.filter(t => t.getur)
+//  }
 
 }
