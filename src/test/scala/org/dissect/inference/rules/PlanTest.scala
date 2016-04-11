9da6ceee0e81e63b53730ef73b4432d4c3631d2b
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-04-11 14:46:46 +0200

    Plan generator playground class.

---
+++ src/test/scala/org/dissect/inference/rules/PlanTest.scala
@@ -0,0 +1,21 @@
+package org.dissect.inference.rules
+
+import org.dissect.inference.utils.RuleUtils
+
+/**
+  * @author Lorenz Buehmann
+  */
+object PlanTest {
+
+
+  def main(args: Array[String]) {
+    val rules = RuleUtils.load("test.rules")
+
+    var rule = RuleUtils.byName(rules, "rdfs2").get
+//    Planner.rewrite(rule)
+
+    rule = RuleUtils.byName(rules, "prp-trp").get
+    Planner.rewrite(rule)
+  }
+
+}
