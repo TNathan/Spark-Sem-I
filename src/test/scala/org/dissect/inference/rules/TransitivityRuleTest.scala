12df90d31ae5274a2d0b1e0807ad0d862c361732
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-04-19 14:56:49 +0200

    Extended test data. Native reasoner joins on multiple variables now.

--- src/test/scala/org/dissect/inference/rules/TransitivityRuleTest.scala
+++ src/test/scala/org/dissect/inference/rules/TransitivityRuleTest.scala
@@ -6,6 +6,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.dissect.inference.data.{RDFGraph, RDFGraphLoader, RDFGraphWriter, RDFTriple}
 import org.dissect.inference.rules.Planner.Plan
 import org.dissect.inference.utils.{RuleUtils, TripleUtils}
+import scala.math.Ordering.Implicits._
 
 import scala.collection.mutable
 
@@ -29,6 +30,7 @@ object TransitivityRuleTest {
     val ns = "http://ex.org/"
     val p1 = ns + "p1"
     val p2 = ns + "p2"
+    val p3 = ns + "p3"
     triples += RDFTriple(p1, RDF.`type`.getURI, OWL2.TransitiveProperty.getURI)
     triples += RDFTriple(p2, RDF.`type`.getURI, OWL2.TransitiveProperty.getURI)
 
@@ -40,6 +42,11 @@ object TransitivityRuleTest {
     for(i <- 11 to 20) { // should not produce (?x_i, p1, ?z_i) as p1 and p2 are used
       triples += RDFTriple(ns + "x" + i, p1, ns + "y" + i)
       triples += RDFTriple(ns + "y" + i, p2, ns + "z" + i)
+    }
+
+    for(i <- 21 to 30) { // should not produce (?x_i, p3, ?z_i) as p3 is not transitive
+      triples += RDFTriple(ns + "x" + i, p3, ns + "y" + i)
+      triples += RDFTriple(ns + "y" + i, p3, ns + "z" + i)
     }
 
     val triplesRDD = sc.parallelize(triples.toSeq, 2)
@@ -75,40 +82,80 @@ object TransitivityRuleTest {
       plan.triplePatterns.foreach{tp =>
 //        println(tp)
         val rel = graph.find(tp)
-//        println("REL\n" + rel.collect().mkString("\n"))
+        println("REL\n" + rel.collect().mkString("\n"))
         relations += (tp -> rel)
       }
 
       // TODO order joins by dependencies
-      plan.joins.foreach{join =>
-        println("JOIN: " + join)
+      val joins = plan.joins
 
-        val rel1 = relations(join.tp1)
-        val rel2 = relations(join.tp2)
+      // we merge joins with the same triple patterns
+      val mergedJoins = mergeJoins(joins)
 
-        val joinVar = join.joinVar
+      mergedJoins.foreach{entry =>
 
-        // convert RDD of relation 1 by position of join variable
-        val tmp1 = TripleUtils.position(joinVar, join.tp1) match {
-          case 1  => RDDOperations.subjKeyPredObj(rel1)
-          case 2  => RDDOperations.predKeySubjObj(rel1)
-          case 3  => RDDOperations.objKeySubjPred(rel1)
-        }
-        println("TMP1\n" + tmp1.collect().mkString("\n"))
+        val tp1 = entry._1._1
+        val tp2 = entry._1._2
 
-        // convert RDD of relation 1 by position of join variable
-        val tmp2 = TripleUtils.position(joinVar, join.tp2) match {
-          case 1  => RDDOperations.subjKeyPredObj(rel2)
-          case 2  => RDDOperations.predKeySubjObj(rel2)
-          case 3  => RDDOperations.objKeySubjPred(rel2)
-        }
-        println("TMP2\n" + tmp2.collect().mkString("\n"))
+        val joinVars = entry._2.map(join => join.joinVar)
 
-        // perform join
-        val res = tmp1.join(tmp2)
+        println("JOIN: " + tp1 + " JOIN " + tp2 + " ON " + joinVars)
+
+        val rel1 = relations(tp1)
+        val rel2 = relations(tp2)
+
+        val res =
+          if (joinVars.size == 1) {
+            // convert RDD of relation 1 by position of join variables
+            val tmp1 =
+              TripleUtils.position(joinVars.head, tp1) match {
+                case 1 => RDDOperations.subjKeyPredObj(rel1)
+                case 2 => RDDOperations.predKeySubjObj(rel1)
+                case 3 => RDDOperations.objKeySubjPred(rel1)
+              }
+            println("TMP1\n" + tmp1.collect().mkString("\n"))
+
+            // convert RDD of relation 2 by position of join variables
+            val tmp2 =
+              TripleUtils.position(joinVars.head, tp2) match {
+                case 1 => RDDOperations.subjKeyPredObj(rel2)
+                case 2 => RDDOperations.predKeySubjObj(rel2)
+                case 3 => RDDOperations.objKeySubjPred(rel2)
+              }
+            println("TMP2\n" + tmp2.collect().mkString("\n"))
+
+            // perform join
+            tmp1.join(tmp2)
+          } else {
+            // convert RDD of relation 1 by position of join variables
+            val positions1 = Seq(joinVars.map(v => TripleUtils.position(v, tp1)).toList).map(tup => tup(0) -> tup(1)).head
+            val tmp1 =
+              positions1 match {
+                case (1, 2) => RDDOperations.subjPredKeyObj(rel2)
+                case (1, 3) => RDDOperations.subjObjKeyPred(rel2)
+                case (2, 3) => RDDOperations.objPredKeySubj(rel2)
+              }
+            println("TMP1\n" + tmp1.collect().mkString("\n"))
+
+            // convert RDD of relation 2 by position of join variables
+            val positions2 = Seq(joinVars.map(v => TripleUtils.position(v, tp2)).toList).map(tup => tup(0) -> tup(1)).head
+            val tmp2 =
+              positions2 match {
+                case (1, 2) => RDDOperations.subjPredKeyObj(rel2)
+                case (1, 3) => RDDOperations.subjObjKeyPred(rel2)
+                case (2, 3) => RDDOperations.objPredKeySubj(rel2)
+              }
+            println("TMP2\n" + tmp2.collect().mkString("\n"))
+
+            // perform join
+            tmp1.join(tmp2)
+          }
         println("RES\n" + res.collect().mkString("\n"))
-
       }
+    }
+
+    def mergeJoins(joins: mutable.Set[Planner.Join]) = {
+      joins.groupBy(join => (join.tp1, join.tp2))
     }
   }
 
