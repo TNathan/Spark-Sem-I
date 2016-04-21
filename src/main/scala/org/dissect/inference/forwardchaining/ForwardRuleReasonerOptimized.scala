f7c4669ca0fd14f08e56a02dd002c19d5deb313d
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-04-21 15:15:31 +0200

    Skeleton for optimized reasoner.

---
+++ src/main/scala/org/dissect/inference/forwardchaining/ForwardRuleReasonerOptimized.scala
@@ -0,0 +1,70 @@
+package org.dissect.inference.forwardchaining
+
+import org.apache.jena.reasoner.rulesys.Rule
+import org.apache.spark.SparkContext
+import org.dissect.inference.data.RDFGraph
+import org.dissect.inference.rules.RuleExecutorNative
+import org.slf4j.LoggerFactory
+
+import scala.language.{existentials, implicitConversions}
+
+/**
+  * A naive implementation of the forward chaining based reasoner.
+  *
+  * @author Lorenz Buehmann
+  */
+class ForwardRuleReasonerOptimized(sc: SparkContext, rules: Set[Rule]) extends ForwardRuleReasoner{
+
+  private val logger = com.typesafe.scalalogging.slf4j.Logger(LoggerFactory.getLogger(this.getClass.getName))
+
+  val ruleExecutor = new RuleExecutorNative(sc)
+
+  /**
+    * Applies forward chaining to the given RDF graph and returns a new RDF graph that contains all additional
+    * triples based on the underlying set of rules.
+    *
+    * @param graph the RDF graph
+    * @return the materialized RDF graph
+    */
+  def apply(graph: RDFGraph): RDFGraph = {
+
+    var currentGraph = graph.cache()
+
+    var oldCount = 0L
+    var nextCount = currentGraph.size
+    do {
+      oldCount = nextCount
+
+      currentGraph = currentGraph
+        .union(applyRules(graph))
+        .cache()
+      nextCount = currentGraph.size()
+    } while (nextCount != oldCount)
+
+    graph
+  }
+
+  /**
+    * Apply a set of rules on the given graph.
+    *
+    * @param graph the graph
+    */
+  def applyRules(graph: RDFGraph): RDFGraph = {
+    var newGraph = graph
+    rules.foreach {rule =>
+      newGraph = newGraph.union(applyRule(rule, graph))
+    }
+    newGraph
+  }
+
+  /**
+    * Apply a single rule on the given graph.
+    *
+    * @param rule the rule
+    * @param graph the graph
+    */
+  def applyRule(rule: Rule, graph: RDFGraph): RDFGraph = {
+    logger.debug("Rule:" + rule)
+    ruleExecutor.execute(rule, graph)
+  }
+}
