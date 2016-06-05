87e42d12a37caffafcc159ab9fb4e96f251a17cb
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-06-05 15:46:02 +0200

    Schema extractor for RDFS.

--- src/main/scala/org/dissect/inference/data/RDFTriple.scala
+++ src/main/scala/org/dissect/inference/data/RDFTriple.scala
@@ -7,10 +7,12 @@ package org.dissect.inference.data
   */
 case class RDFTriple(subject: String, predicate: String, `object`: String) extends Product3[String, String, String] {
   override def _1: String = subject
-
   override def _2: String = predicate
-
   override def _3: String = `object`
+
+  def s = subject
+  def p = predicate
+  def o = `object`
 
   override def toString = subject + "  " + predicate + "  " + `object`
 }
